80eba096ec23ad09987b9f15506c4b02b5c24b80
MUHAMADRAFLIPRATAMA <kipxrap@gmail.com>
2025-05-26 16:17:53 +0700

    cetak daftar dokter

--- asosiasi/asosiation.cpp
+++ asosiasi/asosiation.cpp
@@ -37,3 +37,12 @@ public :
     daftar_dokter.push_back(pDokter);
 }
 
+void pasien:: cetakDokter () {
+    cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+    for (auto& a : daftar_dokter) { 
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+
+}
+
